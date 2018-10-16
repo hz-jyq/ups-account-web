0d27b635e436b2eeafefb3867645a39b7c031964
xuxu <xuxucainiao001@163.com>
2018-10-16 14:53:21 +0800

    ok

--- ups-account-web/src/main/java/com/pgy/account/web/UpsAccountWebApplication.java
+++ ups-account-web/src/main/java/com/pgy/account/web/UpsAccountWebApplication.java
@@ -1,19 +1,17 @@
 package com.pgy.account.web;
 
-
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-
 @SpringBootApplication
-@EnableTransactionManagement(proxyTargetClass=true)
+@EnableTransactionManagement(proxyTargetClass = true)
 @MapperScan("com.pgy.account.web.dao")
 public class UpsAccountWebApplication {
 
-	public static void main(String[] args) {		
-		SpringApplication.run(UpsAccountWebApplication.class, args); 
+	public static void main(String[] args) {
+		SpringApplication.run(UpsAccountWebApplication.class, args);
 	}
-			
+
 }
