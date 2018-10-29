6f452225680a5afa92963c232c418304880d702e
jinyuanqian <jinyuanqian@letto8.com>
2018-10-29 18:53:27 +0800

    fix

--- ups-account-web/src/main/java/com/pgy/account/web/UpsAccountWebApplication.java
+++ ups-account-web/src/main/java/com/pgy/account/web/UpsAccountWebApplication.java
@@ -1,5 +1,7 @@
 package com.pgy.account.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +10,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UpsAccountWebApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(UpsAccountWebApplication.class);
+
 	public static void main(String[] args) {
+		logger.info ("begin to start-up [ups-biz]");
 		SpringApplication.run(UpsAccountWebApplication.class, args);
+		logger.info ("start-up [ups-biz] success !!!");
 	}	
 
 }
