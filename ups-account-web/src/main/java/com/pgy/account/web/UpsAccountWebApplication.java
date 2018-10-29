93d23f474bbc95c21b6b5124ccc27b8ce8b34c09
xuxu <xuxucainiao001@163.com>
2018-10-29 18:17:44 +0800

    10.29

--- ups-account-web/src/main/java/com/pgy/account/web/UpsAccountWebApplication.java
+++ ups-account-web/src/main/java/com/pgy/account/web/UpsAccountWebApplication.java
@@ -3,7 +3,11 @@ package com.pgy.account.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/**
+ * UPS账务系统启动
+ * @author 墨凉
+ *
+ */
 
 @SpringBootApplication
 public class UpsAccountWebApplication {
